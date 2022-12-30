0ca1abf0611f43bf229054dba744d550c84a9fd3
Rita Zaborowska <rm.zaborowska@gmail.com>
2022-12-30 12:57:40 +0100

    Adding for loop for first page titles

--- index.js
+++ index.js
@@ -9,4 +9,18 @@ const playwright = require('playwright');
     await page.waitForTimeout(1000); // wait for 1 seconds
     await page.locator('button:text("OK, ZGADZAM SIĘ")').click();
     await page.waitForTimeout(5000);
+    const title1 = await page.textContent("//article[1]/div/div/div[2]/div[1]/h2/a")
+    console.log(title1)
+    const title2 = await page.textContent("//article[2]/div/div/div[2]/div[1]/h2/a")
+    console.log(title2)
+    const title3 = await page.textContent("//article[3]/div/div/div[2]/div[1]/h2/a")
+    console.log(title3)
+    const title4 = await page.textContent("//article[70]/div/div/div[2]/div[1]/h2/a")
+    console.log(title4)
+    const title5 = await page.textContent("//article[68]/div/div/div[2]/div[1]/h2/a")
+    console.log(title5)
+    for (let i = 1; i < 71; i++) {
+        const title = await page.textContent(`//article[${i}]/div/div/div[2]/div[1]/h2/a`)
+        console.log(title,i)
+    }
 })();
